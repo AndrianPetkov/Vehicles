28a9d2aeeefd9f634eb61510feca310cc9bd629e
AndrianPetkov <60959251+AndrianPetkov@users.noreply.github.com>
2021-03-10 11:47:10 +0200

    Add files via upload

---
+++ VehiclesSolution/Vehicles/Utilities/ExceptionMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Vehicles.Utilities
+{
+    public static class ExceptionMessages
+    {
+        public const string InvalidStringAmount = "Cannot fit {0} fuel in the tank";
+
+        public const string NegativeFuelAmount = "Fuel must be a positive number";
+    }
+}
